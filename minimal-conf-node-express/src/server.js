474d57296814f89393c41f72595b68e4bf281013
santiloopz <santilpzate@gmail.com>
2021-01-23 14:32:07 +0100

    added method for mpa

--- minimal-conf-node-express/src/server.js
+++ minimal-conf-node-express/src/server.js
@@ -6,8 +6,23 @@ const fs = require('fs');
 // With this, we need to append the /static path to all our static HTML imports
 app.use('/static', express.static(path.resolve(__dirname, '../dist')));
 
+// SPA
 app.get('/', (req, res) => {
 	const htmlFilePath = path.resolve(__dirname, '../dist/universe.html');
+	const contentFromHTMLFile = fs.readFileSync(htmlFilePath, 'utf-8');
+
+	res.send(contentFromHTMLFile);
+});
+
+// MPA
+app.get('/universe/', (req, res) => {
+	const htmlFilePath = path.resolve(__dirname, '../dist/universe.html');
+	const contentFromHTMLFile = fs.readFileSync(htmlFilePath, 'utf-8');
+
+	res.send(contentFromHTMLFile);
+});
+app.get('/star/', (req, res) => {
+	const htmlFilePath = path.resolve(__dirname, '../dist/star.html');
 	const contentFromHTMLFile = fs.readFileSync(htmlFilePath, 'utf-8');
 
 	res.send(contentFromHTMLFile);
