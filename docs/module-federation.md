1b0461d16abe7be88852c7f79e434a97455db6e2
santiloopz <santilpzate@gmail.com>
2021-01-27 08:43:10 +0100

    adding detailed isntructions on module federation

--- docs/module-federation.md
+++ docs/module-federation.md
@@ -4,4 +4,29 @@ Useful when dealing with an ecosystem of multiple applications.
 Module Federation allows one application to load modules from another application at runtime.
 
 ### How to
-You can start by creating two separate `Webpack` applications, both of them with different ports.
+You can start by creating two separate `Webpack` applications, both of them with different ports. It is important to note that `Module Federation` is only available from `webpack 5` and on. To make sure you can apply run:
+```
+yarn add webpack@latest webpack-cli@latest
+```
+
+### Set up
+First import `webpack module federation plugin`:
+```javascript
+// Import module federation plugin
+const { ModuleFederationPlugin } = require('webpack').container;
+```
+Confugure the plugin, adding it to the list of plugins:
+```javascript
+new ModuleFederationPlugin({
+	name: 'ModuleFederationPluginExample',
+	// contains all that this webpack bundle will export to the rest of the world
+	filename: 'remoteEntry.js',
+	exposes: {
+		'./ModuleName': './Path/To/Mopdule/Module.js', 
+	}
+}),
+```
+Note that now `webpack` needs to know the `public path` from where the app will be available, in order to serve the files:
+```javascript
+
+```
---
+++ minimal-conf-module-federation/webpack.dev.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// Import module federation plugin
+const { ModuleFederationPlugin } = require('webpack').container;
+
+
+module.exports = {
+	entry: {
+		universe: './src/universe.js',
+		star: './src/star.js',
+	},
+	output: {
+		filename: '[name].js',
+		path: path.resolve(__dirname, './dist'),		
+	},
+	mode: 'development',
+	devServer: {
+		contentBase: path.resolve(__dirname, './dist'),
+		index: 'index.html',
+	},
+	module: {
+		rules: [
+			{
+				test: /\.hbs$/,
+				use: ['handlebars-loader'],
+			},
+			{
+				test: /\.(s*)css$/,
+				use: ['style-loader', 'css-loader', 'sass-loader'],
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: ['babel-loader'],
+			},
+		],
+	},
+	plugins: [
+		// Terser plugin included by default in production
+		new HTMLWebpackPlugin({
+			template: 'src/page-template.hbs',
+			filename: 'universe.html',
+			title: 'Universe Generator',
+			description: 'Create a Universe',
+			// There are the chunks specified in the entry object
+			chunks: ['universe'],
+		}),
+		new HTMLWebpackPlugin({
+			template: 'src/page-template.hbs',
+			filename: 'star.html',
+			title: 'Star Generator',
+			description: 'Create a Start',
+			// There are the chunks specified in the entry object
+			chunks: ['star'],
+		}),
+		new CleanWebpackPlugin({
+			// Here you can specify different folders that you want to clean up, apart from dist
+			cleanOnceBeforeBuildPatters: [
+				// remove all files in dist folder
+				'**/*',
+				// remove all files in build folder
+				path.join(process.cwd(), 'build/**/*'),
+			],
+		}),
+		new ModuleFederationPlugin({
+			name: 'ModuleFederationPluginExample',
+			// contains all that this webpack bundle will export to the rest of the world
+			filename: 'remoteEntry.js',
+			exposes: {
+				'./ModuleName': './Path/To/Mopdule/Module.js', 
+			}
+		}),
+	],
+}
