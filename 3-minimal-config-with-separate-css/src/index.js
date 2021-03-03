4230d304e85cbf7913c9aab2fcc6464367f963e2
santiloopz <santilpzate@gmail.com>
2021-03-03 08:18:11 +0100

    WIP Configure with separate CSS

--- 3-minimal-config-with-separate-css/src/index.js
+++ 3-minimal-config-with-separate-css/src/index.js
@@ -1,6 +1,7 @@
 import connect from './connect.js';
 import CreateUniverse from './components/create-universe/create-universe.js';
 
+// TODO this is broken
 connect();
 const createUniverse = new CreateUniverse();
 createUniverse.render();
